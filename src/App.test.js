2a18c72fe579610144d99cd80e1a716f897bda23
yomarguti <yomar.guti@gmail.com>
2020-07-09 22:15:15 -0500

    Initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -7,3 +7,9 @@ test('renders learn react link', () => {
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('Second test suite', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
